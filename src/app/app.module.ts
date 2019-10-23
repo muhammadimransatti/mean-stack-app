cf8b07678e1594800c472663c831809129c6cfe8
muhammadimransatti <mimransatti5@gmail.com>
2019-10-23 13:53:41 -0400

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import {FormControl, Validators} from '@angular/forms';
+import { MatInputModule,
+   MatCardModule,
+   MatButtonModule,
+   MatToolbarModule,
+   MatExpansionModule
+  } from '@angular/material';
+import { PostsService } from './posts/posts.service';
+import {HttpClientModule } from "@angular/common/http";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    HttpClientModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit} from '@angular/core';
+import { NgForm} from "@angular/forms";
+import { PostsService } from '../posts.service';
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent implements OnInit {
+
+enteredId = '';  
+enteredTitle = '';
+enteredContent = '';
+enteredEmail = '';
+
+
+
+onAddPost(form: NgForm){
+  if(form.invalid){
+    return ;
+  }
+this.postsService.addPost(form.value.title, form.value.content , form.value.email);
+form.resetForm();
+}
+
+
+
+  constructor(public postsService: PostsService) { }
+
+  ngOnInit() {
+  }
+
+}
